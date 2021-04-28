69b3d662a1d8e0e744be795991ef8fda3b673a7e
Alberto Navalón <albertonl.dev@gmail.com>
2021-04-28 12:01:29 +0200

    add face camera

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
+  <Camera/>
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Camera from './components/Camera.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Camera
   }
 }
 </script>
